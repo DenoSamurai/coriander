5130757b4faadac2eebed7ab02b82571531beea0
DenoSamurai <livegrow20161101@gmail.com>
2020-09-22 07:15:21 +0900

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_flutter_app/main_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                        model.kboyText,
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        model.changeKboyText();
+                      },
+                    )
+                  ],
+                ),
+              );
+            }
+          ),
+
+        ),
+      ),
+    );
+  }
+}
